130cce68c28b221299834d5758f3399217f13ba4
Pliskin <nabil14@gmail.com>
2022-03-24 21:42:24 +0100

    setup descriptions

--- plugins/matchsetup.py
+++ plugins/matchsetup.py
@@ -6,18 +6,21 @@ Created on Mon Mar 21 15:08:26 2022
 """
 
 import random
+import discord
 from discord.ext import commands
 import configparser
+import json
 
 def split_config_list(value):
     return [x.strip() for x in value.split(',')]
 
 class matchsetup(commands.Cog):
     
-    def __init__(self, bot, config = None):
+    def __init__(self, bot, config = None, desc = None):
         print("Match setup plugin started.")
         self.bot = bot
         self.config = config
+        self.desc = desc
         
     def parse_command(self, args):
         option = ""
@@ -56,7 +59,21 @@ class matchsetup(commands.Cog):
             value = cardinal
         if (value > 0):
             choice = choices[random.randint(0, value-1)]
-        await ctx.send(choice)
+        
+        dicts = [ _dict for _dict in self.desc
+                 if ("title" in _dict and _dict["title"] == choice) ]
+        nb_desc = len(dicts)
+        desc = {}
+        if (nb_desc >= 1):
+            desc = dicts[random.randint(0, nb_desc-1)]
+        
+        embed = discord.Embed.from_dict(desc)
+        if ("title" in desc and "category" in desc):
+            embed.title = "Random " + desc["category"] + ": " + desc["title"]
+        
+        embed.colour = discord.Colour.random()
+        
+        await ctx.send(embed=embed)
         
         if (len(left_args)):
             await self.setup(ctx, *left_args)
@@ -64,4 +81,9 @@ class matchsetup(commands.Cog):
 def setup(bot):
     config = configparser.ConfigParser()
     config.read('config/setup.ini')
-    bot.add_cog(matchsetup(bot, config))
+    
+    json_file = open('config/setup_descriptions.json', encoding="utf8")
+    desc = json.load(json_file)
+    json_file.close()
+    
+    bot.add_cog(matchsetup(bot, config, desc))
